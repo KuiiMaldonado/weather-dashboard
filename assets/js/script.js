f390d91367613ae1f74e2ac0dbbfbfbd9eae330a
unknown <cuitlahuacjr@gmail.com>
2022-04-20 18:43:16 -0500

    Search hisotry saved in localStorage with a max of 8 elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const API_URL = 'https://api.openweathermap.org/';
-const search_form = document.getElementById('search_form');
+const searchForm = document.getElementById('search_form');
+const searchCity = document.getElementById('search_city');
 
 var city = { };
+var searchHistory = [];
 
 function getCityCoordinates(searchCityText) {
 
@@ -36,12 +38,35 @@ function getCityWeather() {
     });
 }
 
+function saveSearchHistory(cityName) {
+
+    let savedHistory = JSON.parse(localStorage.getItem('searchHistory'));
+
+    if (savedHistory === null) {
+        let searchArray = [];
+        searchArray.push(cityName);
+        // console.log(searchArray);
+        localStorage.setItem('searchHistory', JSON.stringify(searchArray));
+    }
+    else {
+        if(savedHistory.length < 8) {
+            savedHistory.push(cityName);
+        }
+        else {
+            savedHistory.unshift(cityName);
+            savedHistory.pop();
+        }
+        localStorage.setItem('searchHistory', JSON.stringify(savedHistory));
+    }
+}
 
 function submitHandler(event) {
 
     event.preventDefault();
     let searchCityText = document.getElementById('search_city').value;
+    saveSearchHistory(searchCityText)
     getCityCoordinates(searchCityText);
+    searchCity.value = ''
 }
 
-search_form.addEventListener('submit', submitHandler);
+searchForm.addEventListener('submit', submitHandler);
